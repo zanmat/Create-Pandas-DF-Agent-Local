8c9c0e033d49ba82300be568c374b1decd85852b
Mattia Zanazzi <mattiazanazzi@gmail.com>
2024-07-02 17:37:57 -0500

    Adding script and data

---
+++ agent.py
@@ -0,0 +1,30 @@
+from langchain.agents.agent_types import AgentType
+from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
+from langchain_openai import ChatOpenAI
+from langchain_community.chat_models import ChatOllama
+
+
+import pandas as pd
+from langchain_openai import OpenAI
+
+
+df = pd.read_csv(
+   "data/data.csv"
+)
+
+
+#agent = create_pandas_dataframe_agent(OpenAI(temperature=0), df, verbose=True, allow_dangerous_code=True)
+
+
+agent = create_pandas_dataframe_agent(
+   ChatOllama(temperature=0, model="mistral"),
+   df,
+   verbose=True,
+#    agent_type=AgentType.OPENAI_FUNCTIONS,
+   allow_dangerous_code=True
+)
+
+
+agent.invoke("how many refugees were there in Argentina and Brazil in 2022?")
+
+
